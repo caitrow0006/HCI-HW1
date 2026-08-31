e7223743bc5a20ac73dd778e4460d77b436a5667
lsouth <lsouth269@gmail.com>
2026-08-31 10:01:45 -0400

    First commit

---
+++ hw1.py
@@ -0,0 +1,16 @@
+# TODO: Edit these variables so they store your name and your hometown.
+my_name = "Laura South"
+my_hometown = "Denver, CO"
+
+print(f"Hello, my name is {my_name} and I am from {my_hometown}")
+# TODO: Write a print statement here that explains why you chose to take this course. 
+print()
+
+# TODO: Write a print statement here that describes how comfortable you are programming in Python
+print()
+
+# TODO: Write a print statement here that describes how comfortable you are writing code in HTML and JavaScript
+print()
+
+# TODO: Write a print statement here that describes how comfortable you are with writing assignments (in English!)
+print()
