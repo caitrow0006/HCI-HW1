58ba74a7f210d9be4c4c845c29f1138112797707
Caitriona Row <caitrionarow19@gmail.com>
2026-09-02 08:54:36 -0400

    modifications to HW1

--- hw1.py
+++ hw1.py
@@ -1,16 +1,16 @@
 # TODO: Edit these variables so they store your name and your hometown.
-my_name = "Laura South"
-my_hometown = "Denver, CO"
+my_name = "Caitriona M Row"
+my_hometown = "Amherst, NH"
 
 print(f"Hello, my name is {my_name} and I am from {my_hometown}")
 # TODO: Write a print statement here that explains why you chose to take this course. 
-print()
+print("I chose to take this course for two main reasons. I designed the UI for my group's CS619 OO-programming project, and I would like to learn how the choices I made were either good or bad from a design perspective. Additionally, I need a final writing intensive course in order to graduate.")
 
 # TODO: Write a print statement here that describes how comfortable you are programming in Python
-print()
+print("I am not very comfortable programming in Python. I transferred into the BS in CS major and thus never took CS415 or CS410P. Instead I took CS410C. Additionally, I use C/C++ in my job, so I have not had a chance to practice Python since high school. That being said, I am confident in my ability to pick up the skills I need for this course. I will do what is necessary to succeed.")
 
 # TODO: Write a print statement here that describes how comfortable you are writing code in HTML and JavaScript
-print()
+print("I am comfortable writing in HTML, though I do not enjoy it. I am not comfortable writing in JavaScript, as I have never done so before. However, my previous statement still applies.")
 
 # TODO: Write a print statement here that describes how comfortable you are with writing assignments (in English!)
-print()
+print("I am very comfortable writing assignements in English. English is my native tongue and I do not forsee any issues with my ability to read and write in English.")
